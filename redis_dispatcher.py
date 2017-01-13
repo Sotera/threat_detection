17b583ca5a3534f107c79566f788b692e288e29e
Luke Wendling <luke@lukewendling.com>
2017-01-13 13:40:35 -0600

    update redis-dispatcher

--- redis_dispatcher.py
+++ redis_dispatcher.py
@@ -1,25 +1,26 @@
 '''
 What does this module do?
-1. Creates pubsub client and listen for messages.
+1. Receives job params from a Redis list.
 2. Kicks off a client-defined long-running job.
 3. Updates redis hash when job completes.
 '''
 
 from __future__ import print_function
-import redis, sys
+import redis, sys, traceback
 
 '''
 Fetch job data and call processing function
 '''
 class Worker(object):
-    def __init__(self, redis_store):
-        self.send = redis_store
+    def __init__(self, redis_client):
+        self.send = redis_client
 
     '''
     run worker with given msg and args
     '''
     def run(self, msg, **kwargs):
-        key = msg['data']
+        print(msg)
+        key = msg[1]
         initial_state = kwargs.get('initial_state', 'new')
         process_func = kwargs.get('process_func',
             lambda key, job: sys.stdout.write('processing placeholder'))
@@ -27,7 +28,6 @@ class Worker(object):
         # get object for corresponding key:
         job = self.send.hgetall(key)
         print(job)
-        print(key)
         if not job:
             self.send.hmset(key, {'state': 'error', 'error': 'could not find item in redis'})
             print('COULD NOT FIND ITEM IN REDIS')
@@ -52,34 +52,35 @@ class Worker(object):
             job['state'] = 'error'
             job['error'] = e
             print(e)
+            traceback.print_exc()
         # when done, update job
         self.send.hmset(key, job)
 
 '''
-Listen for messages and dispatch workers.
-1 worker per message for parallelization support (TODO).
+Listen for job params and dispatch workers.
 '''
 class Dispatcher(object):
     '''
     Args:
-        channels: list of channel names
+        queue: redis list to pull jobs from
         process_func: a blocking function with args: key (string), job (dict)
         initial_state: of redis job. ex. 'new'
     '''
     def __init__(self, redis_host='localhost', redis_port=6379, **kwargs):
         self.redis_host = redis_host
         self.redis_port = redis_port
-        self.channels = kwargs['channels']
+        self.queues = kwargs['queues']
         self.process_func = kwargs['process_func']
         self.initial_state = kwargs.get('initial_state', 'new')
 
     def start(self):
+        # decode_responses: retain char encoding from publisher
         pool = redis.ConnectionPool(host=self.redis_host,
             port=self.redis_port, decode_responses=True)
-        redis_store = redis.Redis(connection_pool=pool)
+        redis_client = redis.Redis(connection_pool=pool)
         try:
             # test connection
-            redis_store.ping()
+            redis_client.ping()
         except redis.exceptions.ConnectionError as rexc:
             print('''
             Cannot connect to host "{host}" port {port}.
@@ -87,22 +88,15 @@ class Dispatcher(object):
             '''.format(host=self.redis_host, port=self.redis_port))
             print(rexc)
             return
-        redis_subscriber = redis.Redis(connection_pool=pool)
-        pubsub = redis_subscriber.pubsub()
-        pubsub.subscribe(self.channels)
-        # listen for messages and do work:
-        for item in pubsub.listen():
-            print('MESSAGE HEARD')
-            if item['type'] == 'subscribe': # subscribe response
-                print('SUBSCRIBED TO CHANNEL %s' % item['channel'])
-            elif item['data'] == 'KILL':
-                pubsub.unsubscribe()
-                print('un-subscribed and finished')
-                break
-            else:
-                worker = Worker(redis_store)
-                try:
+        print('WATCHING QUEUES %s' % self.queues)
+        while True:
+            try:
+                # block for 10 sec and reset (is blocking forever ok?)
+                item = redis_client.brpop(self.queues, 10)
+                if item:
+                    print('MESSAGE HEARD')
+                    worker = Worker(redis_client)
                     worker.run(item, process_func=self.process_func, initial_state=self.initial_state)
-                except Exception as e:
-                    print('error running redis worker:', e)
-
+            except Exception as e:
+                print('error running redis worker:', e)
+                traceback.print_exc()
